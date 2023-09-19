a471c03d144acfcdb2780e7919e621080d379278
Alex <2733908676@qq.com>
2023-09-19 15:34:38 +0800

    dotenv的使用

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 const Koa = require('koa')
 
+const { APP_PORT } = require('./config/config.default')
+
 const app = new Koa()
 
 app.use((ctx, next) => {
   ctx.body = 'hello koa-api'
 })
 
-app.listen(3000, () => {
-  console.log('Serve is running on http://localhost:3000')
+app.listen(APP_PORT, () => {
+  console.log(`Serve is running on http://localhost:${APP_PORT}`)
 })
