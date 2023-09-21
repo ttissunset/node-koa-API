f304cc6884cd2010452a180162f249d4b4cc0c61
Alex <2733908676@qq.com>
2023-09-21 09:12:00 +0800

    创建数据库链接

---
+++ ReadME.md
@@ -0,0 +1,3 @@
+# node-koa-API
+通过koa框架搭建API服务
+
---
+++ src/db/seq.js
@@ -0,0 +1,27 @@
+const { Sequelize } = require('sequelize')
+
+// 导入配置文件
+const {
+  MYSQL_HOST,
+  MYSQL_PORT,
+  MYSQL_USER,
+  MYSQL_PWD,
+  MYSQL_DB,
+} = require('../config/config.default')
+
+const seq = new Sequelize(MYSQL_DB, MYSQL_USER, MYSQL_PWD, {
+  host: MYSQL_HOST,
+  dialect: 'mysql',
+})
+
+// 测试mysql是否链接成功
+seq
+  .authenticate()
+  .then(() => {
+    console.log('数据库连接成功')
+  })
+  .catch((err) => {
+    console.error('Unable to connect to the database:', err)
+  })
+
+module.exports = seq
