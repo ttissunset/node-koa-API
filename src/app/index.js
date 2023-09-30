f19b1ba580977afb60eb840b4477759732416541
Alex <2733908676@qq.com>
2023-09-30 12:08:16 +0800

    商品图片上传

--- src/app/index.js
+++ src/app/index.js
@@ -1,15 +1,32 @@
+const path = require('path')
+
 const Koa = require('koa')
 
 const errHandler = require('./error.handle')
 
 // 引入解析body参数的中间件并注册
-const { koaBody } = require('koa-body')
+const{ koaBody} = require('koa-body')
+const koaStaic = require('koa-static')
 
 const app = new Koa()
 
 const router = require('../router')
 
-app.use(koaBody())
+app.use(
+  koaBody({
+    // multipart:是否开启文件上传 --> 默认为false
+    multipart: true,
+    formidable: {
+      // 文件要保存的路径
+      //! 在配置选项option中，应该避免使用相对路径 --> 因为在option中，相对的是 process.cwd() 而非当前文件
+      uploadDir: path.join(__dirname, '../upload'),
+      // 保留文件后缀名
+      keepExtensions: true,
+    },
+  })
+)
+// 使用 koa-static 中间件将upload文件夹配置为静态资源
+app.use(koaStaic(path.join(__dirname, '../upload')))
 app.use(router.routes()).use(router.allowedMethods())
 
 // 统一的错误处理
--- src/controller/goods.controller.js
+++ src/controller/goods.controller.js
@@ -1,6 +1,26 @@
+const Path = require('path')
+
+const { fileUploadError } = require('../constant/error.type')
+const { log } = require('console')
+
 class GoodsController {
   async upload(ctx, next) {
-    ctx.body = '文件上传成功！！'
+    // 从  ctx.request.files 中解构出 名称为 file 的文件
+    const { file } = ctx.request.files
+    console.log(file);
+    // 判断是否有名称为 file 的文件 
+    if(file){
+      ctx.body = {
+        code:0,
+        message:'文件上传成功！！',
+        result:{
+          goods_img:Path.basename(file.filepath)
+        }
+      }
+    }else{
+      // 如果没有则抛出错误
+      return ctx.app.emit('error',fileUploadError,ctx)
+    }
   }
 }
 
