ad51ffe5dbf4e98562fe41ff52e9e0e799dd296b
Alex <2733908676@qq.com>
2023-10-16 18:58:13 +0800

    图片上传类型判断

--- src/controller/goods.controller.js
+++ src/controller/goods.controller.js
@@ -1,25 +1,30 @@
 const Path = require('path')
 
-const { fileUploadError } = require('../constant/error.type')
+const { fileUploadError,unSupportedFileType } = require('../constant/error.type')
 const { log } = require('console')
 
 class GoodsController {
   async upload(ctx, next) {
     // 从  ctx.request.files 中解构出 名称为 file 的文件
     const { file } = ctx.request.files
-    console.log(file);
-    // 判断是否有名称为 file 的文件 
-    if(file){
-      ctx.body = {
-        code:0,
-        message:'文件上传成功！！',
-        result:{
-          goods_img:Path.basename(file.filepath)
-        }
+    // 指定文件上传类型为jpg/png
+    const fileType = ['image/jpg', 'image/png']
+    // 判断是否有名称为 file 的文件
+    if (file) {
+      if (!fileType.includes(file.types)) {
+        // 如果文件类型不属于我们指定的文件类型，则抛出错误
+        return ctx.app.emit('error', unSupportedFileType, ctx)
       }
-    }else{
+      ctx.body = {
+        code: 0,
+        message: '文件上传成功！！',
+        result: {
+          goods_img: Path.basename(file.filepath),
+        },
+      }
+    } else {
       // 如果没有则抛出错误
-      return ctx.app.emit('error',fileUploadError,ctx)
+      return ctx.app.emit('error', fileUploadError, ctx)
     }
   }
 }
